5a4c2687d98725d8f2c16da1e783c2dfff8e09b4
Belhadj Ahmed  Walid <80895522+BAW2501@users.noreply.github.com>
2023-02-07 00:10:20 +0000

    guide tree

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import skbio
-import scipy as sp
-
+from functools import partial
+from scipy.cluster.hierarchy import average, dendrogram, to_tree
+from skbio import DistanceMatrix
 from skbio import DNA
+
 
 def kmer_distance(sequence1, sequence2, k=7, overlap=False):
     sequence1_kmers = set(map(str, sequence1.iter_kmers(k, overlap)))
@@ -11,7 +12,7 @@ def kmer_distance(sequence1, sequence2, k=7, overlap=False):
     number_unique = len(all_kmers) - len(shared_kmers)
     fraction_unique = number_unique / len(all_kmers)
     return fraction_unique
-   
+
 
 filenames = [
     "APOE Variant 1.txt",
@@ -47,3 +48,26 @@ print(kmer_distance(HouseM2, HouseM1, k=7, overlap=False))
 print(HumanA.distance(HouseM1, kmer_distance))
 print(HouseM1.distance(HouseM2, kmer_distance))
 
+fivemer_distance = partial(kmer_distance, k=5)
+
+s1 = DNA("ACCGGTGACCAGTTGACCAGT")
+s2 = DNA("ATCGGTACCGGTAGAAGT")
+s3 = DNA("GGTACCAAATAGAA")
+
+print(s1.distance(s2, fivemer_distance))
+print(s1.distance(s3, fivemer_distance))
+
+query_of_sequences = [DNA(BrownRat, {"id": "s1"}),
+                      DNA(HouseM1, {"id": "s2"}),
+                      DNA(HouseM2, {"id": "s3"}),
+                      DNA(HumanA, {"id": "s4"})
+                      ]
+
+guide_dm = DistanceMatrix.from_iterable(query_of_sequences, metric=kmer_distance, key='id')
+
+guide_lm = average(guide_dm.condensed_form()) # upgma
+print(guide_dm)
+guide_d = dendrogram(guide_lm, labels=guide_dm.ids, orientation='right',link_color_func=lambda x: 'black')
+print(guide_d)
+guide_tree = to_tree(guide_lm)
+print(guide_tree)
